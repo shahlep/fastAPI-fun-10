31af3355fc2fc39a82606607f8941e099c225fa6
Shah rahman <toshah.rahman@gmail.com>
2022-06-18 12:07:00 +0300

    fun step 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 def index():
     return {"Message": "Hello World"}
+
+
+@app.post("/create_posts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"Title: {payload['title']} Content: {payload['content']}"}
