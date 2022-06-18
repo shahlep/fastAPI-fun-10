0f68c6a50772456f5aee3403148d0084153019e2
Shah rahman <toshah.rahman@gmail.com>
2022-06-18 12:54:01 +0300

    fun step 5

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title :str
+    content:str
 
 
 @app.get("/")
