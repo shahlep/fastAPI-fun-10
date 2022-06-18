3a8062952e36d6cd71c87e181ab0aee107e50d7c
Shah rahman <toshah.rahman@gmail.com>
2022-06-18 14:05:04 +0300

    fun step 16

--- main.py
+++ main.py
@@ -40,7 +40,14 @@ def get_posts(id: int):
             return p
 
 
+@app.get("/posts/latest")
+def get_latest_post():
+    post= my_posts[len(my_posts)-1]
+    return {"detail":post}
+
 @app.get("/posts/{id}")
 def get_posts_by_id(id: int):
     post = get_posts(id)
     return {"Posts": post}
+
+
