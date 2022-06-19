ce7e7d8becda2dd36b5087eb584da4bda8760365
Shah rahman <toshah.rahman@gmail.com>
2022-06-19 10:16:06 +0300

    fun step 33

--- config/settings.py
+++ config/settings.py
@@ -4,3 +4,13 @@ from dotenv import load_dotenv
 
 env_path = Path("") / ".env"
 load_dotenv(dotenv_path=env_path)
+
+class Settings:
+    POSTGRESS_USER: str = os.getenv("POSTGRESS_USER")
+    POSTGRESS_PASSWORD: str = os.getenv("POSTGRESS_PASSWORD")
+    POSTGRESS_SERVER: str = os.getenv("POSTGRESS_SERVER", "localhost")
+    POSTGRESS_PORT: str = os.getenv("POSTGRESS_PORT", 5432)
+    POSTGRESS_DB: str = os.getenv("POSTGRESS_DB")
+    DATABASE_URL = f"postgresql://{POSTGRESS_USER}:{POSTGRESS_PASSWORD}@{POSTGRESS_SERVER}:{POSTGRESS_PORT}/{POSTGRESS_DB}"
+
+settings = Settings()
