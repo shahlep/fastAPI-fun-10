af63a9f2210113df61f5c21e0775c426daa58f49
Shah rahman <toshah.rahman@gmail.com>
2022-06-19 15:57:33 +0300

    fun step 79

--- database.py
+++ database.py
@@ -10,3 +10,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
