12ac87db913afcb171ee7db1210aefcb94b13d05
Shah rahman <toshah.rahman@gmail.com>
2022-06-19 15:26:13 +0300

    fun step 73

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from config.settings import Settings
 
 SQLALCHEMY_DATABASE_URL = Settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False,autocommit=False)
