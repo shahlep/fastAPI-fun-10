cab1b29469a64bfb7a16b7fc0de42384f3afe4b5
Shah rahman <toshah.rahman@gmail.com>
2022-06-19 15:23:27 +0300

    fun step 70

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
+from config.settings import Settings
+
+SQLALCHEMY_DATABASE_URL = Settings.DATABASE_URL
