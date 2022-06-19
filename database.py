40ed7e4f2891decd252b458f336d9c4421a7182a
Shah rahman <toshah.rahman@gmail.com>
2022-06-19 15:51:04 +0300

    fun step 74

--- database.py
+++ database.py
@@ -8,3 +8,5 @@ SQLALCHEMY_DATABASE_URL = Settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False,autocommit=False)
+
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,10 @@
+from .database import Base
+from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy.sql.expression import null
+
+class Post(Base):
+    id = Column(Integer,primary_key=True,nullable=False)
+    title = Column(String,nullable=False)
+    content = Column(String,nullable=False)
+    published = Column(Boolean,default=True)
+
