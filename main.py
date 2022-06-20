9ab40c68da0f7c1c338511ee71c9c080c1cc04d1
Shah rahman <toshah.rahman@gmail.com>
2022-06-20 06:40:17 +0300

    fun step 85

--- main.py
+++ main.py
@@ -47,37 +47,44 @@ def index():
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute(
-        """INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",
-        (
-            post.title,
-            post.content,
-            post.published,
-        ),
-    )
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post,db:Session=Depends(get_db)):
+    #cursor.execute(
+     #   """INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",
+     #   (
+      #      post.title,
+       #     post.content,
+       #     post.published,
+       # ),
+    #)
+    #new_post = cursor.fetchone()
+    #conn.commit()
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"new_post": new_post}
 
 
 @app.get("/posts")
-def get_all_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
+def get_all_posts(db:Session=Depends(get_db)):
+    #cursor.execute("""SELECT * FROM posts""")
+    #posts = cursor.fetchall()
+    posts=db.query(models.Post).all()
     return {"Posts": posts}
 
 
 @app.get("/posts/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts) - 1]
+def get_latest_post(db:Session=Depends(get_db)):
+    #post = my_posts[len(my_posts) - 1]
+    post = db.query(models.Post).order_by(sorted(reverse=False))
     return {"detail": post}
 
 
 @app.get("/posts/{id}")
-def get_posts_by_id(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
-    post = cursor.fetchone()
+def get_posts_by_id(id: int,db:Session=Depends(get_db)):
+    #cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    #post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found"
@@ -86,34 +93,41 @@ def get_posts_by_id(id: int):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING * """, (str(id)))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-
-    if deleted_post is None:
+def delete_post(id: int,db:Session=Depends(get_db)):
+    #cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING * """, (str(id)))
+    #deleted_post = cursor.fetchone()
+    #conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id {id} doesn't exist",
         )
+
+    post.delete()
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title=%s,content=%s,published=%s WHERE id=%s RETURNING *""",
-        (post.title, post.content, post.published, str(id)),
-    )
-    updated_post = cursor.fetchone()
-    conn.commit()
+def update_post(id: int,updated_post:Post,db:Session=Depends(get_db)):
+    #cursor.execute(
+     #   """UPDATE posts SET title=%s,content=%s,published=%s WHERE id=%s RETURNING *""",
+      #  (post.title, post.content, post.published, str(id)),
+    #)
+    #updated_post = cursor.fetchone()
+    #conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if updated_post is None:
+    if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id {id} doesn't exist",
         )
-    return {"Updated Post": updated_post}
+    post_query.update(updated_post.dict())
+    db.commit()
+    return {"Updated Post": post_query.first()}
 
 
 @app.get("/sqlalchemy")
