d755997ecfa9122c1f0f1e9584819d9c8db3931b
Shah rahman <toshah.rahman@gmail.com>
2022-06-21 20:12:47 +0300

    fun step 154

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ class Settings:
 
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    ACCESS_TOKEN_EXPIRE_MINUTES:int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 settings = Settings()
--- oauth2.py
+++ oauth2.py
@@ -1,18 +1,15 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
-
-SECRET_KEY = "8f0e552a31e2e8023389819b24508c5b3b13451f655be5b3e219ce0cfd330ca5"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from config.settings import Settings
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now() + timedelta(minutes=Settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     to_encode.update()
 
     encoded_jwt = jwt.encode(
-        to_encode, SECRET_KEY, algorithm=ALGORITHM
+        to_encode, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM
     )
     return encoded_jwt
