219c6e594de24ecc78e096cbc3c9d21e684db7ee
Shah rahman <toshah.rahman@gmail.com>
2022-06-21 17:42:57 +0300

    fun step 144

--- routers/auth.py
+++ routers/auth.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter,Depends,status,HTTPException
 from sqlalchemy.orm import Session
 from database import get_db
+
+router = APIRouter(tags=["Authentication"])
+
+@router.post("/login")
+def login(db:Session=Depends(get_db)):
+    pass
