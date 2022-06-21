e123a2dcca8f85ea3143d54219337e86a0da544b
Shah rahman <toshah.rahman@gmail.com>
2022-06-21 16:58:42 +0300

    fun step 138

--- routers/posts.py
+++ routers/posts.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException, Response
 import schemas as _schemas, models as _models
 from sqlalchemy.orm import Session
 from database import get_db
+from typing import List
 
 router = APIRouter()
 
