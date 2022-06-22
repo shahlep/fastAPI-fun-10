c4aabd26a5aa0593495f732a4b5ce727a8a5f75f
Shah rahman <toshah.rahman@gmail.com>
2022-06-22 17:12:15 +0300

    fun step 177

--- oauth2.py
+++ oauth2.py
@@ -10,18 +10,18 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=Settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=Settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(
-        to_encode, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM
+        to_encode, Settings.SECRET_KEY, algorithm=[Settings.ALGORITHM]
     )
     return encoded_jwt
 
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, Settings.SECRET_KEY, algorithms=Settings.ALGORITHM)
+        payload = jwt.decode(token, Settings.SECRET_KEY, algorithms=[Settings.ALGORITHM])
         id: str = payload.get("user_id")
 
         if id is None:
