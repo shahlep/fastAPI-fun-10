0a9705e8cfbda32cfb7a35d5ff301f23e864f929
Shah rahman <toshah.rahman@gmail.com>
2022-06-23 18:43:53 +0300

    fun step 204

--- routers/posts.py
+++ routers/posts.py
@@ -9,9 +9,9 @@ router = APIRouter(prefix="/posts", tags=["Posts"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=_schemas.ShowPost)
 def create_posts(
-    post: _schemas.PostCreate,
-    db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.get_current_user),
+        post: _schemas.PostCreate,
+        db: Session = Depends(get_db),
+        current_user: int = Depends(oauth2.get_current_user),
 ):
     # cursor.execute(
     #   """INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",
@@ -31,18 +31,20 @@ def create_posts(
 
 
 @router.get("/", response_model=List[_schemas.ShowPost])
-def get_all_posts(
-    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
-):
+def get_all_posts(limit: int=None,
+                  db: Session = Depends(get_db),
+                  current_user: int = Depends(oauth2.get_current_user),
+
+                  ):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    posts = db.query(_models.Post).filter(_models.Post.owner_id==current_user.id).all()
+    posts = db.query(_models.Post).filter(_models.Post.owner_id == current_user.id).limit(limit).all()
     return posts
 
 
 @router.get("/latest", response_model=_schemas.ShowPost)
 def get_latest_post(
-    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
+        db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
 ):
     # post = my_posts[len(my_posts) - 1]
     post = db.query(_models.Post).order_by(_models.Post.id.desc())
@@ -51,9 +53,9 @@ def get_latest_post(
 
 @router.get("/{id}", response_model=_schemas.ShowPost)
 def get_posts_by_id(
-    id: int,
-    db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.get_current_user),
+        id: int,
+        db: Session = Depends(get_db),
+        current_user: int = Depends(oauth2.get_current_user),
 ):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
     # post = cursor.fetchone()
@@ -67,15 +69,15 @@ def get_posts_by_id(
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(
-    id: int,
-    db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.get_current_user),
+        id: int,
+        db: Session = Depends(get_db),
+        current_user: int = Depends(oauth2.get_current_user),
 ):
     # cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING * """, (str(id)))
     # deleted_post = cursor.fetchone()
     # conn.commit()
     post_query = db.query(_models.Post).filter(_models.Post.id == id)
-    post= post_query.first()
+    post = post_query.first()
     if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -94,10 +96,10 @@ def delete_post(
 
 @router.put("/{id}", response_model=_schemas.ShowPost)
 def update_post(
-    id: int,
-    updated_post: _schemas.PostCreate,
-    db: Session = Depends(get_db),
-    current_user: int = Depends(oauth2.get_current_user),
+        id: int,
+        updated_post: _schemas.PostCreate,
+        db: Session = Depends(get_db),
+        current_user: int = Depends(oauth2.get_current_user),
 ):
     # cursor.execute(
     #   """UPDATE posts SET title=%s,content=%s,published=%s WHERE id=%s RETURNING *""",
