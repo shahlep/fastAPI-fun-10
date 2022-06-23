b66ffe014032d8f09f07532ff94b4a8330be7fec
Shah rahman <toshah.rahman@gmail.com>
2022-06-23 07:01:19 +0300

    fun step 191

--- models.py
+++ models.py
@@ -13,7 +13,10 @@ class Post(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
-    user_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE") , nullable=False)
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+
 
 class User(Base):
     __tablename__ = "users"
