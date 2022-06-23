3c05f1c8c7c0853370df000915582f2db7d5e8c2
Shah rahman <toshah.rahman@gmail.com>
2022-06-23 16:52:49 +0300

    fun step 193

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql.expression import text
 
 class Post(Base):
     __tablename__ = "posts"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False,index=True)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default="True", nullable=False)
@@ -20,7 +20,7 @@ class Post(Base):
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False,index=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(
