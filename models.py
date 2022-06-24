5fbf31bfa62dc76233ee7a2aadc4cb2d8d4c3909
Shah rahman <toshah.rahman@gmail.com>
2022-06-24 09:57:56 +0300

    fun step 213

--- models.py
+++ models.py
@@ -29,6 +29,11 @@ class User(Base):
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
 
+
 class Vote(Base):
-    user_id = Column(Integer,ForeignKey("users.id", ondelete="CASCADE"),primary_key=True)
-    post_id=Column(Integer,ForeignKey("posts.id", ondelete="CASCADE"),primary_key=True)
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+    )
