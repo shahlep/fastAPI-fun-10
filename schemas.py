2b8d216aa56829930558faa5dd2524d64fa28e3e
Shah rahman <toshah.rahman@gmail.com>
2022-06-24 10:35:23 +0300

    fun step 216

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr,conint
+from pydantic import BaseModel, EmailStr, conint
 from datetime import datetime
 from typing import Optional
 
@@ -50,6 +50,7 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     id: Optional[str] = None
 
+
 class Vote(BaseModel):
-    post_id:int
-    dir:conint(le=1)
+    post_id: int
+    dir: conint(le=1)
