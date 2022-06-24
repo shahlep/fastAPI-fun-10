8b4dd9e6e0922067b22303fac1d85da0c168124b
Shah rahman <toshah.rahman@gmail.com>
2022-06-24 10:39:28 +0300

    fun step 219

--- routers/vote.py
+++ routers/vote.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 import models as _models, schemas as _schemas, oauth2
 from database import get_db
+
+
+router = APIRouter(prefix="/vote",tags=["Vote"])
