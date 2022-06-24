a93312deb8f695f0c0f1253961ba3305500d26bc
Shah rahman <toshah.rahman@gmail.com>
2022-06-24 10:42:44 +0300

    fun step 221

--- routers/vote.py
+++ routers/vote.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 import models as _models, schemas as _schemas, oauth2
 from database import get_db
+from sqlalchemy.orm import Session
 
 
 router = APIRouter(prefix="/vote", tags=["Vote"])
+
+@router.post("/",status_code=status.HTTP_201_CREATED)
+def create_vote(vote:_schemas.Vote,
+                db:Session=Depends(get_db),
+                current_user:int=Depends(oauth2.get_current_user))
+    pass
