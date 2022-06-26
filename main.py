de603a58bc7531b34b355d41f36eee6d0c5b5880
Shah rahman <toshah.rahman@gmail.com>
2022-06-26 10:06:54 +0300

    fun step 230

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from database import engine
 from routers.users import router as _users
 from routers.posts import router as _posts
 from routers.auth import router as _auth
-
+from routers.vote import router as _vote
 
 app = FastAPI()
 
@@ -15,6 +15,7 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(_users)
 app.include_router(_posts)
 app.include_router(_auth)
+app.include_router(_vote)
 
 
 @app.get("/")
