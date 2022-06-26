7a3a7bdfed730783500c01ed6061913cab99189b
Shah rahman <toshah.rahman@gmail.com>
2022-06-26 09:54:41 +0300

    fun step 227

--- routers/vote.py
+++ routers/vote.py
@@ -13,6 +13,9 @@ def create_vote(
     db: Session = Depends(get_db),
     current_user: int = Depends(oauth2.get_current_user),
 ):
+    vote_query =db.query(_models.Vote).filter(
+        _models.Vote.post_id==vote.post_id,_models.Vote.user_id==current_user.id)
+    found_vote = vote_query.first()
     if vote.dir == 1:
         pass
     else:
