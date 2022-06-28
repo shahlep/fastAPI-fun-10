8c45c2545b81437f13af296471ae4e18466bbb2a
Shah rahman <toshah.rahman@gmail.com>
2022-06-28 18:17:13 +0300

    fun step 261

---
+++ alembic/versions/f8274cad2e1c_create_users_table.py
@@ -0,0 +1,24 @@
+"""create users table
+
+Revision ID: f8274cad2e1c
+Revises: 8560c6245b7c
+Create Date: 2022-06-28 18:16:50.046357
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f8274cad2e1c'
+down_revision = '8560c6245b7c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
