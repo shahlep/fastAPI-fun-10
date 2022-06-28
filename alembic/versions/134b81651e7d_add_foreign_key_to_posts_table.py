aa6a9d3d9671d00f3b8af20cd3e5bbd6e0c90228
Shah rahman <toshah.rahman@gmail.com>
2022-06-28 18:32:38 +0300

    fun step 264

---
+++ alembic/versions/134b81651e7d_add_foreign_key_to_posts_table.py
@@ -0,0 +1,24 @@
+"""add foreign key to posts table
+
+Revision ID: 134b81651e7d
+Revises: f8274cad2e1c
+Create Date: 2022-06-28 18:32:21.957255
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '134b81651e7d'
+down_revision = 'f8274cad2e1c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
