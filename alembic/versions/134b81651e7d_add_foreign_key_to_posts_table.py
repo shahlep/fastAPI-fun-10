658a68141212e106fbfa16043d559ba398ffbf77
Shah rahman <toshah.rahman@gmail.com>
2022-06-28 18:36:20 +0300

    fun step 265

--- alembic/versions/134b81651e7d_add_foreign_key_to_posts_table.py
+++ alembic/versions/134b81651e7d_add_foreign_key_to_posts_table.py
@@ -17,8 +17,15 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.add_column("posts",
+                  sa.Column("owner_id",sa.Integer,nullable=False))
+    op.create_foreign_key("posts_users_fk",
+                          source_table="posts",referent_table="users",
+                          local_cols="owner_id",remote_cols="id",
+                          ondelete="CASCADE")
     pass
 
 
 def downgrade() -> None:
+    op.drop_column("posts","owner_id")
     pass
