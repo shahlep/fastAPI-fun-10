8072302c78990f0a375e05932b345bf422b4ca03
Shah rahman <toshah.rahman@gmail.com>
2022-06-28 18:37:54 +0300

    fun step 267

--- alembic/versions/134b81651e7d_add_foreign_key_to_posts_table.py
+++ alembic/versions/134b81651e7d_add_foreign_key_to_posts_table.py
@@ -10,23 +10,26 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '134b81651e7d'
-down_revision = 'f8274cad2e1c'
+revision = "134b81651e7d"
+down_revision = "f8274cad2e1c"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column("posts",
-                  sa.Column("owner_id",sa.Integer,nullable=False))
-    op.create_foreign_key("posts_users_fk",
-                          source_table="posts",referent_table="users",
-                          local_cols="owner_id",remote_cols="id",
-                          ondelete="CASCADE")
+    op.add_column("posts", sa.Column("owner_id", sa.Integer, nullable=False))
+    op.create_foreign_key(
+        "posts_users_fk",
+        source_table="posts",
+        referent_table="users",
+        local_cols="owner_id",
+        remote_cols="id",
+        ondelete="CASCADE",
+    )
     pass
 
 
 def downgrade() -> None:
-    op.drop_constraint("posts_users_fk","posts")
-    op.drop_column("posts","owner_id")
+    op.drop_constraint("posts_users_fk", "posts")
+    op.drop_column("posts", "owner_id")
     pass
