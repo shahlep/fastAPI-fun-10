51da95a866f3a78ebe5877fdf63fc571d00a17f9
Shah rahman <toshah.rahman@gmail.com>
2022-06-28 18:54:41 +0300

    fun step 272

--- alembic/versions/d095bf8fa912_add_two_more_columns_to_posts_table.py
+++ alembic/versions/d095bf8fa912_add_two_more_columns_to_posts_table.py
@@ -10,31 +10,35 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd095bf8fa912'
-down_revision = '134b81651e7d'
+revision = "d095bf8fa912"
+down_revision = "134b81651e7d"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column("posts",
-                  sa.Column(
-                      "created_at",
-                      sa.TIMESTAMP(timezone=True),
-                      server_default=sa.text("now()"),
-                      nullable=False,
-                  ),)
-    op.add_column("posts",
-                  sa.Column(
-                      "published",
-                      sa.Boolean,
-                      server_default='True',
-                      nullable=False,
-                  ), )
+    op.add_column(
+        "posts",
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+    )
+    op.add_column(
+        "posts",
+        sa.Column(
+            "published",
+            sa.Boolean,
+            server_default="True",
+            nullable=False,
+        ),
+    )
     pass
 
 
 def downgrade() -> None:
-    op.drop_column("posts","created_at")
-    op.drop_column(("posts","published"))
+    op.drop_column("posts", "created_at")
+    op.drop_column(("posts", "published"))
     pass
