11192b59a3aa9790c4cd0889b3fc8f2f1dfc150b
Shah rahman <toshah.rahman@gmail.com>
2022-06-29 18:45:43 +0300

    fun step 284

--- tests/test_users.py
+++ tests/test_users.py
@@ -8,3 +8,11 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Message": "Hello World"}
+
+
+def test_create_user():
+    response = client.post("/users/",json={"email":"test123@example.com",
+                                           "password":"password123"})
+    assert response.status_code ==201
+    assert response.json().get("email") == "test123@example.com"
+
