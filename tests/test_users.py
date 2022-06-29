0b6136bc2cef2ae8b03cf49d51a4fd6e9ea475a4
Shah rahman <toshah.rahman@gmail.com>
2022-06-29 20:10:58 +0300

    fun step 289

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,32 @@
 from fastapi.testclient import TestClient
 from main import app
 from schemas import ShowUser
+from config.settings import Settings
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = Settings.TEST_DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
 
 client = TestClient(app)
-
 
 def test_read_main():
     response = client.get("/")
