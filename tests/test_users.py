4eda50d915ac7042935a0b2eb041e060ce6cc6cf
Shah rahman <toshah.rahman@gmail.com>
2022-07-01 06:48:03 +0300

    fun step 303

--- tests/test_users.py
+++ tests/test_users.py
@@ -27,10 +27,12 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
+
 @fixture
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
+
 
 @fixture
 def client(session):
