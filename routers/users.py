1a4cb62091bd76ef0904558b52539cdf9f7f8ae4
Shah rahman <toshah.rahman@gmail.com>
2022-07-01 17:03:05 +0300

    fun step 310

--- routers/users.py
+++ routers/users.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import Session
 from database import get_db
 from typing import List
 
-router = APIRouter(prefix="/users", tags=["Users"])
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=_schemas.ShowUser)
