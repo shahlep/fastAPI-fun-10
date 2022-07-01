986273c88ad28bed59f59e9ba46acb91ed0e1ff5
Shah rahman <toshah.rahman@gmail.com>
2022-07-01 18:39:03 +0300

    fun step 325

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+from fastapi.testclient import TestClient
+from main import app
+from config.settings import Settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database import get_db, Base
+from pytest import fixture
+
+
+SQLALCHEMY_DATABASE_URL = Settings.TEST_DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+Test_SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+
+# Dependency
+
+
+@fixture(scope="function")
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = Test_SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@fixture(scope="function")
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+@fixture(scope="function")
+def test_user(client):
+    user_data = {
+        "email": "test123@example.com",
+        "password": "password123",
+    }
+    response = client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    new_user = response.json()
+    new_user["password"] = user_data["password"]
+    return new_user
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,4 @@
 from schemas import ShowUser
-from .database import client, session
-from pytest import fixture
-
-
-@fixture(scope="function")
-def test_user(client):
-    user_data = {
-        "email": "test123@example.com",
-        "password": "password123",
-    }
-    response = client.post("/users/", json=user_data)
-    assert response.status_code == 201
-    new_user = response.json()
-    new_user["password"] = user_data["password"]
-    return new_user
 
 
 def test_read_main(client):
