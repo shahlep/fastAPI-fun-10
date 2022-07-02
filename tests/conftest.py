7913329a5d5880370fc787412916f35e4e8d7522
Shah rahman <toshah.rahman@gmail.com>
2022-07-02 12:53:54 +0300

    fun step 340

--- tests/conftest.py
+++ tests/conftest.py
@@ -56,4 +56,4 @@ def test_user(client):
 
 @fixture(scope="function")
 def token(test_user):
-    return create_access_token({"user_id":test_user["id"]})
+    return create_access_token({"user_id": test_user["id"]})
