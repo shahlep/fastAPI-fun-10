1ccd5988480a7c32f7e62578b152a6ab87ef6637
Shah rahman <toshah.rahman@gmail.com>
2022-07-02 12:57:56 +0300

    fun step 344

--- tests/conftest.py
+++ tests/conftest.py
@@ -62,6 +62,6 @@ def token(test_user):
 def authorized_client(client, token):
     client.headers = {
         **client.headers,
-        "Authorization":f"bearer {token}",
+        "Authorization": f"bearer {token}",
     }
     return client
