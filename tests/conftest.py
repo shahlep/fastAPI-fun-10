d576885aff5fa19e9ef8eb619bae1945efcc0522
Shah rahman <toshah.rahman@gmail.com>
2022-07-04 17:13:08 +0300

    fun step 356

--- tests/conftest.py
+++ tests/conftest.py
@@ -70,5 +70,17 @@ def authorized_client(client, token):
 
 @fixture(scope="function")
 def test_posts(test_user, session):
-    posts_data =[]
+    posts_data =[{
+        "title":"first title",
+    "content":"first content",
+    "owner_id":test_user['id']},
+        {
+            "title": "2nd title",
+            "content": "2nd content",
+            "owner_id": test_user['id']},
+        {
+            "title": "3rd title",
+            "content": "3rd content",
+            "owner_id": test_user['id']}
+    ]
     pass
