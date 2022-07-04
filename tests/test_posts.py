0d8a6e575bf3afbcbd1949484225c5799cf4fff4
Shah rahman <toshah.rahman@gmail.com>
2022-07-04 18:08:24 +0300

    fun step 372

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -6,5 +6,8 @@ def test_all_posts(authorized_client, test_posts):
     def validate(post):
         return _schemas.ShowPost(**post)
 
+    posts_map = map(validate,response.json())
+    posts_list = list(posts_map)
+
     assert len(response.json()) == len(test_posts)
     assert response.status_code == 200
