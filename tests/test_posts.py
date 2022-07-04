71abdf7c7a1c840b1827b065b5d076abc5f7cb69
Shah rahman <toshah.rahman@gmail.com>
2022-07-04 17:33:45 +0300

    fun step 370

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,5 @@
 def test_all_posts(authorized_client, test_posts):
     response = authorized_client.get("/posts/")
-    print(response.json())
+    #print(response.json())
+    assert len(response.json()) == len(test_posts)
     assert response.status_code == 200
