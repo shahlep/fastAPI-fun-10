6cd81be2b3a30706fa4baff1cafd4eb28e1feffe
Shah rahman <toshah.rahman@gmail.com>
2022-07-04 17:31:22 +0300

    fun step 369

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,5 +1,4 @@
-
-def test_all_posts(authorized_client,test_posts):
+def test_all_posts(authorized_client, test_posts):
     response = authorized_client.get("/posts/")
     print(response.json())
     assert response.status_code == 200
