8ad15f1f6692c3beb7c78318202b4d9e1c208eb6
Shah rahman <toshah.rahman@gmail.com>
2022-07-05 18:04:09 +0300

    fun step 406

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -60,7 +60,7 @@ def test_create_post_by_authorized_user(
     )
     created_post = _schemas.PostCreate(**response.json())
     assert response.status_code == 201
-    #print(response.json())
+    # print(response.json())
 
     assert created_post.title == title
     assert created_post.content == content
