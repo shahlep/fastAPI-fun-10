a7b3005519dd2c2aa1639d2e17ec3cc58ff10bc0
Shah rahman <toshah.rahman@gmail.com>
2022-07-05 16:53:47 +0300

    fun step 386

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -19,3 +19,14 @@ def test_unauthorized_user_get_all_posts(client, test_posts):
     response = client.get("/posts/")
 
     assert response.status_code == 401
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    response = client.get(f"/posts/{test_posts[0].id}")
+
+    assert response.status_code == 401
+
+def test_unauthorized_user_get_one_post(authorized_client, test_posts):
+    response = authorized_client.get(f"/posts/{test_posts[0].id}")
+
+    assert response.status_code == 200
