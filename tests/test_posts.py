8cbdc82f33df06c2098a4b64fc91a452061a1645
Shah rahman <toshah.rahman@gmail.com>
2022-07-05 16:47:07 +0300

    fun step 382

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -10,6 +10,6 @@ def test_all_posts(authorized_client, test_posts):
     post_map = map(validate, response.json())
     post_list = list(post_map)
 
-    #assert post_list[0].Post.id == test_posts[0].id
+    # assert post_list[0].Post.id == test_posts[0].id
     assert len(response.json()) == len(test_posts)
     assert response.status_code == 200
