932ec200daa77ff8123e10c12a219d26fe06bb87
Shah rahman <toshah.rahman@gmail.com>
2022-07-05 06:35:58 +0300

    fun step 375

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -4,6 +4,8 @@ import schemas as _schemas
 def test_all_posts(authorized_client, test_posts):
     response = authorized_client.get("/posts/")
     #print(response.json())
+    def validate(post):
+        return _schemas.ShowPostVote(post)
 
     assert len(response.json()) == len(test_posts)
     assert response.status_code == 200
