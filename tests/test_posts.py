766ae53832e2d6b66ffd183c503e90ad410da2f0
Shah rahman <toshah.rahman@gmail.com>
2022-07-06 17:45:47 +0300

    fun step 439

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -120,3 +120,15 @@ def test_update_post_with_authorized_user(authorized_client, test_posts):
     assert response.status_code == 200
     assert updated_post.title == data["title"]
     assert updated_post.content == data["content"]
+
+def test_update_other_user_post(authorized_client, test_posts):
+    data = {
+        "title": "Updated title",
+        "content": "Updated content",
+        "id": test_posts[3].id,
+    }
+    response = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+
+    assert response.status_code == 403
+
+
