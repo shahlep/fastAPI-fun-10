0a3c0446803f26968cba892fe712ce25c8a83df5
Shah rahman <toshah.rahman@gmail.com>
2022-07-07 16:45:00 +0300

    fun step 457

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -14,6 +14,7 @@ def test_vote_on_post_by_auth_user(authorized_client, test_posts):
     assert response.status_code == 201
 
 
+
 def test_vote_on_post_twice_by_auth_user(authorized_client, test_posts):
     response = authorized_client.post(
         "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
@@ -29,3 +30,10 @@ def test_vote_on_post_by_unauthorized_user(client, test_posts):
 def test_vote_on_nonexisted_post(authorized_client, test_posts):
     response = authorized_client.post("/vote/", json={"post_id": 12, "dir": 1})
     assert response.status_code == 404
+
+def test_remove_vote_on_not_given_vote_on_post_by_auth_user(authorized_client, test_posts):
+    response = authorized_client.post(
+        "/vote/", json={"post_id": test_posts[3].id, "dir": 0}
+    )
+    assert response.status_code == 404
+
