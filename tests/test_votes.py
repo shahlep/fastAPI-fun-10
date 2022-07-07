9b627c3e0130a1e50434465fda776f3c6af1394c
Shah rahman <toshah.rahman@gmail.com>
2022-07-07 06:46:48 +0300

    fun step 447

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -1,2 +1,5 @@
-def test_vote_on_post_by_auth_user():
-    pass
+def test_vote_on_post_by_auth_user(authorized_client,test_posts):
+    response = authorized_client.post("/vote/",
+                                      json={"post_id":test_posts[3].id,
+                                            "dir":1})
+    assert response.status_code == 201
