1520fd06e983717f18b7472e670e5b1c2cf3be5e
Shah rahman <toshah.rahman@gmail.com>
2022-07-07 16:36:11 +0300

    fun step 454

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -3,7 +3,7 @@ import models as _models
 
 
 @fixture(scope="function")
-def test_vote(authorized_client,test_user,test_posts):
+def test_vote(authorized_client, test_user, test_posts):
     pass
 
 
@@ -20,8 +20,7 @@ def test_vote_on_post_twice_by_auth_user(authorized_client, test_posts):
     )
     assert response.status_code == 409
 
+
 def test_vote_on_post_by_unauthorized_user(client, test_posts):
-    response = client.post(
-        "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
-    )
+    response = client.post("/vote/", json={"post_id": test_posts[3].id, "dir": 1})
     assert response.status_code == 401
