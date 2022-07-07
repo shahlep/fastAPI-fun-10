1a86101cf37929fb289cef4cf0a06abd93be5a82
Shah rahman <toshah.rahman@gmail.com>
2022-07-07 06:48:08 +0300

    fun step 449

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -3,3 +3,9 @@ def test_vote_on_post_by_auth_user(authorized_client, test_posts):
         "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
     )
     assert response.status_code == 201
+
+def test_vote_on_post_twice_by_auth_user(authorized_client, test_posts):
+    response = authorized_client.post(
+        "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
+    )
+    assert response.status_code == 409
