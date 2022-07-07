ab9c2f76f9ac4f5d748d851c86db0f758ecd764b
Shah rahman <toshah.rahman@gmail.com>
2022-07-07 17:02:58 +0300

    fun step 468

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -48,7 +48,9 @@ def test_remove_vote_on_post_by_auth_user(authorized_client, test_posts, test_vo
     )
     assert response.status_code == 201
 
-def test_vote_on_own_post(authorized_client, test_posts,test_vote):
-    response = authorized_client.post("/vote/", json={"post_id":test_posts[3].id, "dir": 1})
-    assert response.status_code == 409
 
+def test_vote_on_own_post(authorized_client, test_posts, test_vote):
+    response = authorized_client.post(
+        "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
+    )
+    assert response.status_code == 409
