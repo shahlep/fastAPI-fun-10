2f1344518138365a7900f959abb9005e6c4ffd91
Shah rahman <toshah.rahman@gmail.com>
2022-07-08 16:42:38 +0300

    fun step 475

--- tests/test_users.py
+++ tests/test_users.py
@@ -50,7 +50,7 @@ def test_incorrect_login_user(client, email, password, status_code):
         "/login",
         data={"username": email, "password": password},
     )
-    print(response.json())
+
     assert response.status_code == status_code
     # assert response.json().get("detail") == "Invalid Credentials!"
 
@@ -64,6 +64,7 @@ def test_get_all_user(authorized_client):
 def test_get_user_by_id(authorized_client):
     response = authorized_client.get(f"/users/1")
 
+    assert response.json().get('email') == "test123@example.com"
     assert response.status_code == 200
 
 
