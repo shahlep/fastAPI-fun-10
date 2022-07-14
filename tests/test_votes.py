bb5f2764687130b610b3e824ed631e062dcb07c4
Shah rahman <toshah.rahman@gmail.com>
2022-07-14 17:00:51 +0300

    fun step 496

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -9,31 +9,31 @@ def test_vote(session, test_user, test_posts):
     session.add(new_vote)
     session.commit()
 
-
+@mark.votes
 def test_vote_on_post_by_auth_user(authorized_client, test_posts):
     response = authorized_client.post(
         "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
     )
     assert response.status_code == 201
 
-
+@mark.votes
 def test_vote_on_post_twice_by_auth_user(authorized_client, test_posts, test_vote):
     response = authorized_client.post(
         "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
     )
     assert response.status_code == 409
 
-
+@mark.votes
 def test_vote_on_post_by_unauthorized_user(client, test_posts):
     response = client.post("/vote/", json={"post_id": test_posts[3].id, "dir": 1})
     assert response.status_code == 401
 
-
+@mark.votes
 def test_vote_on_nonexisted_post(authorized_client, test_posts):
     response = authorized_client.post("/vote/", json={"post_id": 12, "dir": 1})
     assert response.status_code == 404
 
-
+@mark.votes
 def test_remove_vote_on_not_given_vote_on_post_by_auth_user(
     authorized_client, test_posts
 ):
@@ -42,14 +42,14 @@ def test_remove_vote_on_not_given_vote_on_post_by_auth_user(
     )
     assert response.status_code == 404
 
-
+@mark.votes
 def test_remove_vote_on_post_by_auth_user(authorized_client, test_posts, test_vote):
     response = authorized_client.post(
         "/vote/", json={"post_id": test_posts[3].id, "dir": 0}
     )
     assert response.status_code == 201
 
-
+@mark.votes
 def test_vote_on_own_post(authorized_client, test_posts, test_vote):
     response = authorized_client.post(
         "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
